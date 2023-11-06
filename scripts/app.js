94fc5216126180faabb7916ca496cec4160f2e98
Zahid Afsar <zahidtempz@icloud.com>
2023-11-06 09:24:23 -0800

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+
+let BtnChange4 = document.getElementById("BtnChange4");
+
+
+BtnChange4.addEventListener("click", function(e){
+    BtnChange4.innerText = "Event with Wicked CSS"
+    BtnChange4.className =  "floater";
+});
